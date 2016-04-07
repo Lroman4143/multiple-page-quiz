aeba472a3a6b02f18ba58d3c27e44d64a8c41c06
Lindsey Roman <lindsey.roman.geekwise@gmail.com>
2016-04-07 21:26:08 +0000

    button testing

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
+var score = 0;
 document.addEventListener("DOMContentLoaded",function(event){
     var button1 = document.getElementById("button1");
     button1.addEventListener("click",function(event){
         
         window.open("http://geekwise-lindsey-roman.github.io/multiple-page-quiz/question1.html","_self");
-    });
-    var button2 = document.getElementById("button2");
-    button2.addEventListener("click",function(event){
         
-        window.open("http://geekwise-lindsey-roman.github.io/multiple-page-quiz/question2.html","_self");
-    });  
-    
+        var question_1 = document.getElementById("question_1");
+        var question_2 = document.getElementById("question_2");
+        var question_3 = document.getElementById("question_3");
+        
+        question_1.onclick = function(){
+            score = score - 5;
+            question_1.disabled = true;
+            question_1.setAttribute("class", "btn disabled");
+        };
+        
+        
+        question_2.onclick = function(){
+            score = score - 5;
+            question_2.disabled = true;
+            question_2.setAttribute("class", "btn disabled two");
+        };
+        
+        question_3.onclick = function(){
+            question_3.disabled = true;
+            question_2.disabled = true;
+            question_1.disabled = true;
+            
+          question_3.setAttribute("class", "btn disabled");
+          
+          score = score + 10;
+          
+          var submit_1 = document.createElement("button");
+          submit_1.setAttribute("class", "btn");
+          submit_1.textContent = "Next Question";
+        };
+      
+    });
     
 });

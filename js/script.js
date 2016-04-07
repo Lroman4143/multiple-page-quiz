90ecf73f5cd4ef0a2ce01f144f9f3bbccf0617cf
Lindsey Roman <lindsey.roman.geekwise@gmail.com>
2016-04-07 20:35:05 +0000

    Added Questions Pages

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(event){
+    var button = document.getElementById("button");
+    button.addEventListener("click",function(event){
+        
+        window.open("https://multiple-page-quiz-geekwiselindseyroman.c9users.io/question2.html","_self");
+    });
+        
+    
+    
+});

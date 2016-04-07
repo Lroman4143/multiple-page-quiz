d0f3057f20db663d7096ed3223a800441cca3fcd
Lindsey Roman <lindsey.roman.geekwise@gmail.com>
2016-04-07 20:53:28 +0000

    question 1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded",function(event){
     var button1 = document.getElementById("button1");
     button1.addEventListener("click",function(event){
         
-        window.open("http://geekwise-lindsey-roman.github.io/multiple-page-quiz/question2.html","_self");
+        window.open("http://geekwise-lindsey-roman.github.io/multiple-page-quiz/question1.html","_self");
     });
     var button2 = document.getElementById("button2");
     button2.addEventListener("click",function(event){
